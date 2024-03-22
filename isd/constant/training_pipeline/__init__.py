08ac676794775bf9843040994b659847a8410f2d
entbappy <entbappy73@gmail.com>
2024-03-22 20:56:04 +0600

    validation added

--- isd/constant/training_pipeline/__init__.py
+++ isd/constant/training_pipeline/__init__.py
@@ -11,3 +11,15 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_S3_DATA_NAME: str = "isd_data_mini.zip"
 
 DATA_BUCKET_NAME = "isd-data-24"
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["images", "labels", "classes.names", "train.txt", "val.txt"]
--- isd/entity/config_entity.py
+++ isd/entity/config_entity.py
@@ -24,3 +24,15 @@ class DataIngestionConfig:
     )
 
     S3_DATA_NAME = DATA_INGESTION_S3_DATA_NAME
+
+
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, DATA_VALIDATION_DIR_NAME
+    )
+
+    valid_status_file_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_STATUS_FILE)
+
+    required_file_list = DATA_VALIDATION_ALL_REQUIRED_FILES
